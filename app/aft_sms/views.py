a89f1e0dbada9c08c0d0132a87f1ee91e1f86dfd
Samora Machel <samora@me.laptop>
2023-02-03 03:36:37 +0300

    changed the sms model

--- app/aft_sms/views.py
+++ app/aft_sms/views.py
@@ -1,16 +1,17 @@
 from rest_framework import viewsets
 
-from aft_sms.models import RecievedSMS, SentSMS
-from aft_sms.serializers import RecievedSMSSerializer, SentSMSSerializer
+from aft_sms.models import BulkRecipient, BulkSMS
+from aft_sms.serializers import (
+    BulkRecipientSerializer,
+    BulkSMSSerializer,
+)
 
 # Create your views here.
+class BulkRecipientViewSet(viewsets.ModelViewSet):
+    queryset = BulkRecipient.objects.all()
+    serializer_class = BulkRecipientSerializer
 
 
-class SentSMSViewSet(viewsets.ModelViewSet):
-    queryset = SentSMS.objects.all()
-    serializer_class = SentSMSSerializer
-
-
-class RecievedSMSViewSet(viewsets.ModelViewSet):
-    queryset = RecievedSMS.objects.all()
-    serializer_class = RecievedSMSSerializer
+class BulkSMSViewSet(viewsets.ModelViewSet):
+    queryset = BulkSMS.objects.all()
+    serializer_class = BulkSMSSerializer
